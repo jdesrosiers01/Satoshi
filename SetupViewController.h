81d405347de26fc8733d794378d4b2868043b614
llSourcell <llSourcell@gmail.com>
2014-01-08 14:38:29 -0500

    first commit

---
+++ SetupViewController.h
@@ -0,0 +1,13 @@
+//
+//  SetupViewController.h
+//  Satoshi
+//
+//  Created by Jason Ravel on 1/7/14.
+//  Copyright (c) 2014 Jason Ravel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SetupViewController : UIViewController
+
+@end
---
+++ SetupViewController.m
@@ -0,0 +1,38 @@
+//
+//  SetupViewController.m
+//  Satoshi
+//
+//  Created by Jason Ravel on 1/7/14.
+//  Copyright (c) 2014 Jason Ravel. All rights reserved.
+//
+
+#import "SetupViewController.h"
+
+@interface SetupViewController ()
+
+@end
+
+@implementation SetupViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
